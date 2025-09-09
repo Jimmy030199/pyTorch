45916ababaaf98f3d9884b3b52d4f4d41723a7e9
Jimmy030199 <lf2nt0301@gmail.com>
2025-09-09 23:38:53 +0800

    補充

--- lesson1/step1_model.py
+++ lesson1/step1_model.py
@@ -6,6 +6,9 @@ class Model(nn.Module):
         # 呼叫父類別 建構子
         super().__init__()
         self.lin = nn.Linear(in_dim,out_dim)
+        #nn.Module 和 nn.Linear 不是同一層級，
+        #nn.Linear 繼承自 nn.Module。
+        
     # x 是模型的輸入資料 pytorch 會自動把資料丟到forward
     def forward(self,x):
         return self.lin(x)
